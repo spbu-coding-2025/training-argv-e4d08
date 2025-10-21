7495fe076d9ee93b16e03d14e8b46dec861f8517
Дан Кахян <dan.kahyan@mail.ru>
2025-10-21 23:12:24 +0300

    add main.c

---
+++ src/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <getopt.h>
+
+static struct option long_options[] = {
+	{"lower_bound", 1, 0, 'l'},
+	{"upper_bound", 1, 0, 'u'},
+	{0,0,0,0}
+};
+ 
+
+int main(int argc, char **argv) {
+	char c;
+	int lower_bound = -1, upper_bound = -1;
+	while (1) {
+		if ((c = getopt_long(argc, argv, "l:u:", long_options, 0)) == -1) {
+			break;
+		}
+		switch (c) {
+			case 'l':
+				lower_bound = atoi(optarg);
+				printf("lower_bound: %s\n", optarg);
+				break;
+			case 'u':
+				upper_bound = atoi(optarg);
+				printf("upper_bound: %s\n", optarg);
+				break;
+		}
+	}
+	if (lower_bound == -1) {
+		fprintf(stderr, "Flag lower_bound is missing.");
+		return 1;
+	}
+	if (upper_bound == -1) {
+		fprintf(stderr, "Flag upper_bound is missing.");
+		return 1;
+	}
+	if (lower_bound > upper_bound) {
+		fprintf(stderr, "The lower bound is greater than the upper bound.");
+		return 1;
+	}
+	for (int i = lower_bound; i <= upper_bound; ++i) {
+		printf("%d ", i);
+	}
+}
